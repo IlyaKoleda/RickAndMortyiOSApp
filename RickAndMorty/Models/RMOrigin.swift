e8c65db9f8998a6dc554577803db32776694be9e
IlyaKoleda <ilyakoleda@icloud.com>
2023-01-23 22:53:45 +0300

    Create data model

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Илья on 23.01.2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable{
+      let name: String
+      let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Илья on 23.01.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
